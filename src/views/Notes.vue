00b5fd5c57af3dc31cb31b2b7782fb886139efdf
qalska <qalska7@gmail.com>
2021-07-27 23:39:43 +0300

    верстка notes page

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>Notes</div>
+  <div class="notes">
+    <h3 class="mt-5 mb-5 d-flex justify-center notes__h3">My Notes</h3>
+
+    <notes-list />
+  </div>
 </template>
 
 <script>
-export default {
+import NotesList from '@/components/NotesList.vue';
 
+export default {
+  components: {
+    NotesList
+  },
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .notes__h3{
+    font-size: 30px;
+  }
 </style>
